08490fa6317812a6629ed3fd3aeba9d802e32c89
Paritosh Balasubrahmanyam <paritosh.balasubrahmanyam@ingdirect.com.au>
2016-12-08 17:17:51 +1100

    assignemnts

---
+++ Assignments/Assignment.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assignment;
+
+namespace Assignment.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Verify_Divisors_ValidInput()
+        {
+            
+        }
+    }
+}
---
+++ Assignments/Assignments.UnitTests/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Assignments.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void GetDivisors_Input_Valid()
+        {
+            //Arrange
+            var input = 20;
+            var output = "{1,2,4,5,10,20}";
+
+            //Act
+            var result = Assignments.Divisors.GetDivisors(input);
+
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+
+        [TestMethod]
+        public void GetDivisors_Input_Valid_Output_LessDivisors_Fail()
+        {
+            //Arrange
+            var input = 20;
+            var output = "{1,2,4,5,10}";
+
+            //Act
+            var result = Assignments.Divisors.GetDivisors(input);
+
+            //Assert
+            Assert.AreNotEqual(output, result);
+        }
+
+        [TestMethod]
+        public void IsNullOrEmpty_Input_Null_return_True()
+        {
+            //Arrange
+            var output = true;
+
+            //Act
+            var result = Assignments.ExtensionMethods.IsNullOrEmpty(null);
+
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+
+        [TestMethod]
+        public void IsNullOrEmpty_Input_Empty_return_True()
+        {
+            //Arrange
+            var output = true;
+
+            //Act
+            var result = Assignments.ExtensionMethods.IsNullOrEmpty("");
+
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+
+        [TestMethod]
+        public void IsNullOrEmpty_Input_NotEmpty_return_False()
+        {
+            //Arrange
+            var output = false;
+
+            //Act
+            var result = Assignments.ExtensionMethods.IsNullOrEmpty("has some value");
+
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+
+        [TestMethod]
+        public void IsNullOrEmpty_Input_NotEmpty_return_False()
+        {
+            //Arrange
+            var output = false;
+
+            //Act
+            var result = Assignments.ExtensionMethods.IsNullOrEmpty("has some value");
+
+            //Assert
+            Assert.AreEqual(output, result);
+        }
+    }
+}
